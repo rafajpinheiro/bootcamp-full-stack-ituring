7f641701ff4e5683fa7fd2fb4949db0a9f738bf6
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-09 22:50:48 -0300

    calculando a tabuada de 123

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,21 @@
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+
+console.log(numero1*123);
+console.log(numero2*123);
+console.log(numero3*123);
+console.log(numero4*123);
+console.log(numero5*123);
+console.log(numero6*123);
+console.log(numero7*123);
+console.log(numero8*123);
+console.log(numero9*123);
+console.log(numero10*123);
