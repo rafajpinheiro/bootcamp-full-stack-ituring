2f9217a55625dab5dcd8595b4c1c5a31c8535b66
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-09 22:40:10 -0300

    aprendendo template strings

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,10 @@
+const nome = "Rafael Pinheiro";
+const email = "rafaeljpinheiro@gmail.com";
+const profissao = "designer";
+const idade = 43;
+const lugarSonho = "Lençóis Maranhenses";
+
+const msg = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarSonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`;
+
+
+console.log(msg);
