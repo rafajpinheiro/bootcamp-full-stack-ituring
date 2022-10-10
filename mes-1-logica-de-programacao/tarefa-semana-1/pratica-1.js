dbfe047aa63582236eb5dddc06fac7b8fb451bc7
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-09 21:22:56 -0300

    exibindo as variáveis

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -3,3 +3,9 @@ const email = "rafaeljpinheiro@gmailcom";
 const profissao = "designer";
 const idade = 43;
 const lugarSonho = "Lençóis Maranhenses";
+
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(idade);
+console.log(lugarSonho);
