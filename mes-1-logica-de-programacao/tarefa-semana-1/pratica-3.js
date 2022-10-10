d86feea36bbe625da8e1fe0226536d1d822cc58b
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-09 21:53:52 -0300

    verificando os tipos da vairáveis

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+const nome = "Rafael Pinheiro";
+const email = "rafaeljpinheiro@gmailcom";
+const profissao = "designer";
+const idade = 43;
+const lugarSonho = "Lençóis Maranhenses";
+
+console.log(typeof nome == "string");
+console.log(typeof email == "string");
+console.log(typeof profissao == "string");
+console.log(typeof idade == "string");
+console.log(typeof lugarSonho == "string");
