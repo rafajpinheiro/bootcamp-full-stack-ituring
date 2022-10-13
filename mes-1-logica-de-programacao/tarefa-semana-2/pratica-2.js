ac11b518ecf05d0cd30288291bfc162e5501fea4
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-12 22:22:56 -0300

    criando a função da assinatura e corrigindo a função da saudação

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -1,8 +1,7 @@
 function saudacao(comprador){
-    console.log("Olá, " + comprador + "!");
+    let txtSaudacao = `Olá, ${comprador}!`;
+    return txtSaudacao;
 }
-
-saudacao("Rafael");
 
 function mensagem(produto, recebedor, endereco){
    let txtMensagem = `Fizemos a entrega do produto: ${produto}, no endereço ${endereco}, com sucesso!
@@ -14,4 +13,12 @@ Esperamos que você esteja contente com o produto. Caso contrário, fale conosco
     return txtMensagem;
 }
 
+function assinatura(remetente){
+    let txtAssinatura = `Atenciosamente, ${remetente}.`;
+    return txtAssinatura;
+}
+
+
+console.log(saudacao("Rafael"));
 console.log(mensagem("Playstation 5", "Ricardo Antunes", "Av. Borges de Medeiros, 85 - Lagoa"));
+console.log(assinatura("Equipe Submarino"));
