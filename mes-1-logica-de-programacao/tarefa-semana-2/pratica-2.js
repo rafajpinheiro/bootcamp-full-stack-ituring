b4063567f9fb02fe4cb05ef658455a67893d3518
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-12 22:12:51 -0300

    criando a função do texto da mensagem

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -4,3 +4,14 @@ function saudacao(comprador){
 
 saudacao("Rafael");
 
+function mensagem(produto, recebedor, endereco){
+   let txtMensagem = `Fizemos a entrega do produto: ${produto}, no endereço ${endereco}, com sucesso!
+   
+O seu produto foi recebido por: ${recebedor}.
+   
+Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.`;
+    
+    return txtMensagem;
+}
+
+console.log(mensagem("Playstation 5", "Ricardo Antunes", "Av. Borges de Medeiros, 85 - Lagoa"));
