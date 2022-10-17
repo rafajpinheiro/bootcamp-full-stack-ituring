7323b91edf8bc80f0a4bd1fc292085b71ccc1698
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-17 09:24:37 -0300

    implementando arrow functions

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-6.js
@@ -0,0 +1,13 @@
+/*
+let calculaValor = (valorProduto, desconto) => {
+    valorFinal = valorProduto - (valorProduto * (desconto/100));
+    return valorFinal;
+}
+
+console.log(calculaValor(100, 90));
+*/
+
+
+let calculaValor = (valorProduto, desconto) => valorFinal = valorProduto - (valorProduto * (desconto/100));
+
+console.log(calculaValor(100, 90));
