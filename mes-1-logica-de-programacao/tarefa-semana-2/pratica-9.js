dc33f7670082fc35547e2ca5d0a6d7a046ee1c23
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-17 10:39:59 -0300

    verificando qual o maior número

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -0,0 +1,11 @@
+function maiorNumero(a,b){
+    if (a > b){
+        console.log(`O número ${a} é maior que o número ${b}.`);
+    } else if (b > a){
+        console.log(`O número ${b} é maior que o número ${a}.`);
+    } else if (a = b){
+        console.log(`Os números ${a} e ${b} são iguais.`);
+    }
+}
+
+maiorNumero(58, 9);
