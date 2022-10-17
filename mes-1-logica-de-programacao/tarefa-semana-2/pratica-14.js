f56db4de728fd17ed9127d27466eb48067b38475
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-17 19:14:45 -0300

    calculando o IMC

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-14.js
@@ -0,0 +1,15 @@
+let indice = (peso,altura) => {
+    imc = peso/(altura*altura);
+
+    if (imc < 18.5){
+        console.log("Abaixo do Peso");
+    } else if (imc >= 18.5 && imc < 25){
+        console.log("Peso Normal");
+    } else if (imc >= 25 && imc <= 30){
+        console.log("Sobrepeso");
+    } else if (imc > 30){
+        console.log("Obesidade");
+    }
+}
+
+indice(84, 1.76);
