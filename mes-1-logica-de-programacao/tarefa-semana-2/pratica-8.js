c117076d21dc3e5822bc074fdeca6b94c49f88df
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-17 10:21:59 -0300

    verificando se um número é par ou ímpar

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -0,0 +1,9 @@
+let verificaNumero = (numero) => {
+    if (numero % 2 == 0){
+        console.log(`O número ${numero} é par.`);
+    } else {
+        console.log(`O número ${numero} é ímpar.`);
+    }
+}
+
+verificaNumero(41);
