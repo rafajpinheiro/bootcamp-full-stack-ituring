94e0d00ce304baae4b8a2abf2d4dfd6cd7f7a4d4
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-23 05:06:54 -0300

    criando uma função de cálculo de tabuada

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
@@ -0,0 +1,9 @@
+function tabuada (numeroDaTabuada) {
+    let i = 1;
+    while (i < 11){
+    console.log(`${numeroDaTabuada} x ${i} = ` + numeroDaTabuada*i);
+        i++;
+    }
+}
+
+tabuada(8);
