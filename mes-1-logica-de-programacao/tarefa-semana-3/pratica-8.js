0aa2845b0e2177da2f349c32cb88b127bc1e8cfd
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-26 23:19:41 -0300

    corrigindo o cálculo de um extrato simples

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -1,19 +1,20 @@
 let movimentacao = [100, -20, -30, 10, -7, -21, -5];
-let saldo = 0;
-let deposito = 0;
-let retirada = 0;
 
-function verificaValor(lista) {
-    for (let i=0; i<lista.length; i++)
-    saldo += lista[i];
+let extrato = (lista) => {
+    let deposito = 0;
+    let retirada = 0;
+    let saldo = 0;
 
-    for (let i=0; i<lista.length; i++)
-    if (lista[i] > 0) {
+    for (i = 0; i < lista.length; i++)
+    saldo += lista[i];   
+
+    for (i = 0; i < lista.length; i++)
+    if (lista[i] > 0){
         deposito += lista[i];
     } else {
         retirada += lista[i];
-}
-console.log(`O valor total depositado foi de: R$ ${deposito} \nO valor total retirado foi de: R$ ${retirada} \nO saldo final de sua conta foi positivo no valor de R$ ${saldo}`);
+    }
+    console.log(`O valor total depositado foi de: R$ ${deposito} \nO valor total retirado foi de: R$ ${deposito - saldo} \nO saldo final de sua conta foi positivo no valor de R$ ${saldo}`);
 }
 
-console.log(verificaValor(movimentacao));
+extrato(movimentacao);
