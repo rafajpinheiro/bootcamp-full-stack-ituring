d5d4a57474c893486d2f6b8cd3aaf9fbfd3b8b68
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-11-06 22:33:05 -0300

    buscando carro mais próximo

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-15.js
@@ -0,0 +1,44 @@
+let frota = [{
+    motorista: "Thais S.",
+    avaliacao: 9.8,
+    tempoEstimado: 7,
+    veiculo: "Palio",
+    placa: "ABC4321"
+  },
+  {
+    motorista: "Thales Gonçalves",
+    avaliacao: 8.9,
+    tempoEstimado: 4,
+    veiculo: "Celta",
+    placa: "ABC0987"
+  },
+  {
+    motorista: "José Lopes",
+    avaliacao: 9.9,
+    tempoEstimado: 5,
+    veiculo: "Fox",
+    placa: "ABC1234"
+  },
+  {
+    motorista: "Cristina Souza",
+    avaliacao: 9.3,
+    tempoEstimado: 3,
+    veiculo: "Fox",
+    placa: "ABC1234"
+  },
+  {
+    motorista: "Leo Garcia",
+    avaliacao: 8.5,
+    tempoEstimado: 9,
+    veiculo: "Ka",
+    placa: "ABC8765"
+}];
+
+
+let frotaOrdenada = (lista) => {
+    const motoristaProximo = lista.sort((a, b) => a.tempoEstimado - b.tempoEstimado)[0];
+
+    console.log(`${motoristaProximo.motorista} está a caminho com o carro ${motoristaProximo.veiculo} de placa ${motoristaProximo.placa}. Chega em ${motoristaProximo.tempoEstimado} minutos.`);
+};
+  
+frotaOrdenada(frota);
